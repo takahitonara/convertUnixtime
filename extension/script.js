c16b9a869b8a1b9fed2d04e4b14cfc8046777d52
takahitonara <takahitonara1986@gmail.com>
2013-11-07 02:31:34 +0900

    Add UTC time. Display Local time clearly. resolve #6.

--- extension/script.js
+++ extension/script.js
@@ -1,39 +1,55 @@
 chrome.contextMenus.create({
-    "title": "Convert Unixtime to timestamp",
-    "type": "normal",
-    "contexts": ["selection"],
-    "onclick": function(info) {
-        var ts = encodeURIComponent(info.selectionText);
+	"title" : "Convert Unixtime to timestamp",
+	"type" : "normal",
+	"contexts" : ["selection"],
+	"onclick" : function(info) {
+		var ts = encodeURIComponent(info.selectionText);
 
-        if ( ts.match(/^\d{10}$/) ) {
-	        var d = new Date( ts * 1000 );
-	        
-	        // Get date data
-	        var year   = d.getFullYear();
-	        var month  = d.getMonth() + 1;
-	        var day    = d.getDate();
-	        var hour   = d.getHours();
-	        var minute = d.getMinutes();
-	        var second = d.getSeconds();
-	        
-	        // Set output string
-	        var output = '';
-	        
-	        output += year;
-	        output += '-';
-	        output += ( month  < 10 ) ? '0' + month  : month;
-	        output += '-';
-	        output += ( day    < 10 ) ? '0' + day    : day;
-	        output += ' ';
-	        output += ( hour   < 10 ) ? '0' + hour   : hour;
-	        output += ':';
-	        output += ( minute < 10 ) ? '0' + minute : minute;
-	        output += ':';
-	        output += ( second < 10 ) ? '0' + second : second;
+		if (ts.match(/^\d{10}$/)) {
+			var d = new Date(ts * 1000);
 
- 	        alert(output);
-        } else {
-        	alert("The selected string don't matches the Unixtime format.");
-        }
-    }
+			// Get Local date data
+			var yearLocal = d.getFullYear();
+			var monthLocal = d.getMonth() + 1;
+			var dayLocal = d.getDate();
+			var hourLocal = d.getHours();
+			var minuteLocal = d.getMinutes();
+			var secondLocal = d.getSeconds();
+
+			// Get UTC date data
+			var yearUTC = d.getUTCFullYear();
+			var monthUTC = d.getUTCMonth() + 1;
+			var dayUTC = d.getUTCDate();
+			var hourUTC = d.getUTCHours();
+			var minuteUTC = d.getUTCMinutes();
+			var secondUTC = d.getUTCSeconds();
+
+			var outputLocal = dateOutput(yearLocal, monthLocal, dayLocal, hourLocal, minuteLocal, secondLocal);
+			var outputUTC = dateOutput(yearUTC, monthUTC, dayUTC, hourUTC, minuteUTC, secondUTC);
+			
+			alert(outputLocal + " (Local)" + "\n" + outputUTC + " (UTC)");
+		} else {
+			alert("The selected string don't matches the Unixtime format.");
+		}
+	}
 });
+
+function dateOutput(year, month, day, hour, minute, second) {
+	// Set output string
+	var output = '';
+
+	output += year;
+	output += '-';
+	output += (month < 10 ) ? '0' + month : month;
+	output += '-';
+	output += (day < 10 ) ? '0' + day : day;
+	output += ' ';
+	output += (hour < 10 ) ? '0' + hour : hour;
+	output += ':';
+	output += (minute < 10 ) ? '0' + minute : minute;
+	output += ':';
+	output += (second < 10 ) ? '0' + second : second;
+
+	return output;
+}
+
